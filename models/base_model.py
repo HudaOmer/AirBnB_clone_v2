b933f84959cd4b39eeb059660354b2b62daeee36
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-18 08:19:35 +0100

    style fixed?

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
         else:
             self.id = kwargs['id']
-        
+
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        
+
         else:
             kwargs['updated_at'] = kwargs.get('updated_at', datetime.now())
             kwargs['created_at'] = kwargs.get('created_at', datetime.now())
