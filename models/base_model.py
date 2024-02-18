c25ed48b850488d36674073a8e4b128f5376f1b8
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-18 08:29:03 +0100

    try fixd

--- models/base_model.py
+++ models/base_model.py
@@ -7,30 +7,30 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs.get('id'):
-            self.id = str(uuid.uuid4())
-        else:
-            self.id = kwargs['id']
+            """Instatntiates a new model"""
+            if not kwargs.get('id'):
+                self.id = str(uuid.uuid4())
+            else:
+                self.id = kwargs['id']
 
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            if not kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
 
-        else:
-            kwargs['updated_at'] = kwargs.get('updated_at', datetime.now())
-            kwargs['created_at'] = kwargs.get('created_at', datetime.now())
-            if isinstance(kwargs['updated_at'], str):
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if isinstance(kwargs['created_at'], str):
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs['updated_at'] = kwargs.get('updated_at', datetime.now())
+                kwargs['created_at'] = kwargs.get('created_at', datetime.now())
+                if isinstance(kwargs['updated_at'], str):
+                    kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                if isinstance(kwargs['created_at'], str):
+                    kwargs['created_at'] = datetime.strptime(
+                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
 
-            kwargs.pop('__class__', None)
+                kwargs.pop('__class__', None)
 
-            self.__dict__.update(kwargs)
+                self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -52,6 +52,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
 
