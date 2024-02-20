a6897765341ba2e01095e1f0c1622d1ee7bd4e6c
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-20 20:49:12 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Console Module """
+""" Console Module for HBNB """
 import cmd
 import sys
 from models.base_model import BaseModel
@@ -17,7 +17,7 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB console"""
+    """ This class Contains the functionality for the HBNB console"""
 
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
