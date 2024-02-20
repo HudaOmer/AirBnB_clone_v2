4f3bf7caac3a744b80729c2644479960ce4d6345
HudaOmer <hudaoe175@gmail.com>
2024-02-20 08:51:23 -0800

    Solve task 6 modify basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -18,29 +18,21 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs.get('id'):
-            self.id = str(uuid.uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id =  str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datatime.now()
         else:
-            self.id = kwargs['id']
-
-        if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        else:
-            kwargs['updated_at'] = kwargs.get('updated_at', datetime.now())
-            kwargs['created_at'] = kwargs.get('created_at', datetime.now())
-            if isinstance(kwargs['updated_at'], str):
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if isinstance(kwargs['created_at'], str):
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-
-            kwargs.pop('__class__', None)
-
-            self.__dict__.update(kwargs)
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
