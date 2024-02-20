ceb299a882c9ec00ff562f6dd4a422bc51f80861
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-20 20:51:09 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- script that prepares a MySQL server
+-- A script that prepares a MySQL server
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
