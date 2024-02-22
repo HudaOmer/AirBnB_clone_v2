81b6ec2da5d5679ca18f1c288a28cfc122291d2d
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-22 03:06:39 +0300

    Add to reviews

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
+""" this module tests review """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ a testing class for review """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializing """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ Test id type """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ Test id type for user"""
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ Test text type"""
         new = self.value()
         self.assertEqual(type(new.text), str)
