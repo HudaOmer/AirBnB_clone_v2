51e4142345f37e36b9eb9a415b973037adaafe32
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-08 00:02:57 +0200

    Solve task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ import time
 from fabric.api import local
 from fabric.operations import env, put, run
 
-env.hosts = ['44.210.150.159', '35.173.47.15']
+env.hosts = ["52.91.133.80", "54.90.38.203"]
 
 
 def do_pack():
