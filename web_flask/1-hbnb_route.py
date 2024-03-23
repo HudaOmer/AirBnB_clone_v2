79fac56d1c820999cdfa9efb3442a5af4293c5c2
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 00:54:45 +0300

    Solve task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,11 +8,16 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_hbnb():
-    """This class returns a string with helllo hbnb"""
+    """This class returns a string with hello hbnb"""
     return "Hello HBNB!"
 
 
-@app.route(""/hbnb)
+@app.route("/hbnb")
+def hbnb():
+    """This class returns a string hbnb"""
+    return "HBNB"
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
