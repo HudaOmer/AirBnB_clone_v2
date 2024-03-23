b1cb3f2f1582a4be786de0a1bba184b8aa2f234e
HudaOmer <hudaoe175@gmail.com>
2024-03-23 09:05:53 -0700

    Solve task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello_hbnb():
     """This class returns a string with helllo hbnb"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
