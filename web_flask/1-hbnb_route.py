ef11496b4db1be0c83a02b222e81948e9b7a261c
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 01:43:02 +0300

    Solve task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This is my first flask project"""
 from flask import Flask
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -16,7 +15,6 @@ def hello_hbnb():
 def hbnb():
     """This class returns a string hbnb"""
     return "HBNB"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
