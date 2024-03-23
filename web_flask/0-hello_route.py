b613add2237937a7aad6153044c2d19c1869f2fd
HudaOmer <hudaoe175@gmail.com>
2024-03-23 09:03:42 -0700

    Solve task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
+"""This is my first flask project"""
 from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
-def hello_world():
+def hello_hbnb():
+    """This class returns a string with helllo hbnb"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
