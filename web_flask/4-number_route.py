dd73933c2082c0d630cc419c2c50d3a029bc62ab
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 02:49:28 +0300

    Solve task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This is my first flask project"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """This class returns a string with hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """This class returns a string hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """This class returns a string c + some text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """This class returns a string python + some text"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>")
+def n_is_a_number(n):
+    """This class returns a string int n + is number"""
+    return "{:d} is a number".formt(n)
+
+
+if __name__ == '__main__':
+    """Main function"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
