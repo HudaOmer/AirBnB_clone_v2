729696097941a7fe9141dab9bfad5978f291a616
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 02:19:50 +0300

    Solve task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is my first flask project"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """This class returns a string with hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """This class returns a string hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """This class returns a string c + some text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """This class returns a string python + some text"""
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
