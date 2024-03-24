a1b0f56cbc5e84b2f6cea0f5b91b04d93b70dc01
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 10:14:23 +0200

    Solve task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
 """This is my first flask project"""
-from flask import Flask,  render_template
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """This class returns a string with hello hbnb"""
-    return 'Hello HBNB!'
+    """This class returns a string with hello hbnb'"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """This class returns a string hbnb"""
-    return 'HBNB'
+    """This class returns a string with hbnb"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """This class returns c + some text"""
-    return "C " + text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """This class returns a string with c + some text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    """This class returns a string python + some text"""
-    return "Python " + text.replace('_', ' ')
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """This class returns a string Python + some text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def n_is_number(n):
-    """This class returns a string n is a number, n int only"""
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """This class displays html only if n is int"""
-    return render_template('5-number.html', value=n)
+    """This class returns a string n is a number, n int only"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """displays an HTML page only if <n> is an integer"""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
