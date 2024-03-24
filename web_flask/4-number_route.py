9dc800850563fdfada76fe2be97b2a092e1c68cb
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 03:54:05 +0300

    Solve task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,39 +2,38 @@
 """This is my first flask project"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """This class returns a string with hello hbnb"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb")
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """This class returns a string hbnb"""
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route("/c/<text>")
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """This class returns a string c + some text"""
+    """This class returns c + some text"""
     return "C " + text.replace('_', ' ')
 
 
-@app.route("/python")
-@app.route("/python/<text>")
-def python_is_cool(text="is cool"):
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is_cool'):
     """This class returns a string python + some text"""
     return "Python " + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>')
-def n_is_a_number(n):
-    """This class returns a string int n + is number"""
-    return "{} is a number".formt(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_number(n):
+    """This class returns a string n is a number,int only"""
+    return "{:d} is a number".format(n)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    """ The Main Function """
+    app.run(host='0.0.0.0', port=5000)
