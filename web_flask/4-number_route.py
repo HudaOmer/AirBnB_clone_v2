d8baf8b84e329a8c05a8d5a86e89885456799e16
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-24 03:37:05 +0300

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-""" A script that starts a flask web application"""
+"""This is my first flask project"""
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/")
 def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+    """This class returns a string with hello hbnb"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
-    """ Prints a Message when /hbnb is called """
-    return 'HBNB'
+    """This class returns a string hbnb"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>")
 def c_is_fun(text):
-    """ Prints a Message when /c is called """
+    """This class returns a string c + some text"""
     return "C " + text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    """ Prints a Message when /python is called """
+@app.route("/python")
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """This class returns a string python + some text"""
     return "Python " + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def n_is_number(n):
-    """ Prints a Message when /number is called only if n is an integer"""
-    return "{:d} is a number".format(n)
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_a_number(n):
+    """This class returns a string int n + is number"""
+    return "{} is a number".formt(n)
 
-if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
